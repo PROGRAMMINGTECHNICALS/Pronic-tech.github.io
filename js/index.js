50c745b9f3160bd23041b75d1f936d19cff50818
MR PROGRAMMER <119401196+PROGRAMMINGTECHNICALS@users.noreply.github.com>
2023-06-08 17:00:06 +0300

    Pronic-Tech website

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.aut-wrrt', {
+    strings: ['PROGRAMMERS', 'WEB DEVELOPERS','WEB DISIGNERS','GRAPHICS DESIGNERS','ELECTRONIC DEVELOPERS',],
+    typeSpeed: 50,
+    loop: true,
+   
+  });
+  
